1e8da6917664b4b34b86e064744defa9cf85b107
kevinluna1 <kluna1023@gmail.com>
2020-07-28 18:33:18 -0700

    add updated demo

---
+++ demo/dist/CMakeFiles/main.ex.dir/DependInfo.cmake
@@ -0,0 +1,63 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kl748/Tutorials_Classes/pytorch_practice/torchcpp_practice/OfflineGMRES/demo/WritePlotFile.cpp" "/home/kl748/Tutorials_Classes/pytorch_practice/torchcpp_practice/OfflineGMRES/demo/dist/CMakeFiles/main.ex.dir/WritePlotFile.cpp.o"
+  "/home/kl748/Tutorials_Classes/pytorch_practice/torchcpp_practice/OfflineGMRES/demo/main.cpp" "/home/kl748/Tutorials_Classes/pytorch_practice/torchcpp_practice/OfflineGMRES/demo/dist/CMakeFiles/main.ex.dir/main.cpp.o"
+  "/home/kl748/Tutorials_Classes/pytorch_practice/torchcpp_practice/OfflineGMRES/demo/main_driver.cpp" "/home/kl748/Tutorials_Classes/pytorch_practice/torchcpp_practice/OfflineGMRES/demo/dist/CMakeFiles/main.ex.dir/main_driver.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "AMREX_GIT_VERSION=\"20.07-106-g1a4780d8ef5f\""
+  "AMREX_Linux"
+  "AMREX_PARTICLES"
+  "AMREX_SPACEDIM=3"
+  "AMREX_USE_EB"
+  "AMREX_USE_F_INTERFACES"
+  "AMREX_USE_MPI"
+  "AMREX_USE_OMP"
+  "AT_PARALLEL_OPENMP=1"
+  "BL_FORT_USE_UNDERSCORE"
+  "BL_Linux"
+  "BL_SPACEDIM=3"
+  "BL_USE_F_INTERFACES"
+  "BL_USE_MPI"
+  "BL_USE_OMP"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/kl748/Programs/FHDeX/exec/lib_hydro/cmake/dist/include"
+  "/home/kl748/Programs/FHDeX/exec/lib_hydro/cmake/dist/modules"
+  "/home/kl748/Programs/amrex/dist/mod_files"
+  "/home/kl748/Programs/amrex/Src/Base"
+  "/home/kl748/Programs/amrex/Src/Boundary"
+  "/home/kl748/Programs/amrex/Src/AmrCore"
+  "/home/kl748/Programs/amrex/Src/Amr"
+  "/home/kl748/Programs/amrex/Src/EB"
+  "/home/kl748/Programs/amrex/Src/LinearSolvers/MLMG"
+  "/home/kl748/Programs/amrex/Src/LinearSolvers/Projections"
+  "/home/kl748/Programs/amrex/Src/F_Interfaces/Base"
+  "/home/kl748/Programs/amrex/Src/F_Interfaces/AmrCore"
+  "/home/kl748/Programs/amrex/Src/F_Interfaces/Octree"
+  "/home/kl748/Programs/amrex/Src/F_Interfaces/LinearSolvers"
+  "/home/kl748/Programs/amrex/Src/F_Interfaces/Particle"
+  "/home/kl748/Programs/amrex/Src/Particle"
+  "/home/kl748/Programs/amrex/Src/Extern/amrdata"
+  "/opt/intel/compilers_and_libraries_2020.0.166/linux/mpi/intel64/include"
+  "/opt/intel/compilers_and_libraries_2020.0.166/linux/mpi/intel64/include/gfortran/9.1.0"
+  "/home/kl748/Programs/libtorch/include"
+  "/home/kl748/Programs/libtorch/include/torch/csrc/api/include"
+  "/usr/local/cuda/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
