101349b4b0a2b124737f772919d719285b99dadb
Johannes Blaschke <jpblaschke@lbl.gov>
2020-11-14 14:28:09 -0800

    use constructor instead of the make_arg_pack helper function

--- util/test_wrapper.cpp
+++ util/test_wrapper.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 #include "wrapper.h"
 
@@ -11,6 +12,16 @@ void q1(int x) {
 
 double q2(double x, double y) {
     return (x+y)/2.;
+}
+
+
+void q3(std::vector<double> & x) {
+    std::cout << "called q3 with x=[";
+    for (const double & xi : x) {
+        std::cout << xi;
+        if (& xi != & x.back()) std::cout << ", ";
+    }
+    std::cout << "]" << std::endl;
 }
 
 
@@ -28,7 +39,9 @@ public:
     ReturnWrapper(Function func) : Wrapper<F(Args ...)>(func) {}
 
     F operator()(Args && ... args) {
-        arg_pack<Args ...> ap = make_arg_pack(std::move(args) ...);
+        // arg_pack<Args ...> ap = make_arg_pack(args ...);
+        arg_pack<Args ...> ap(args ...);
+
         // wrapper code running before the wrapped function
         auto ret = this->caller(ap);
         // wrapper cude running after the wrapped function
@@ -51,7 +64,9 @@ public:
     VoidWrapper(Function func) : Wrapper<F(Args...)>(func) {}
 
     F operator()(Args && ... args) {
-        arg_pack<Args ...> ap = make_arg_pack(std::move(args) ...);
+        // arg_pack<Args ...> ap = make_arg_pack(args ...);
+        arg_pack<Args ...> ap(args ...);
+
         // wrapper code running before the wrapped function
         this->caller(ap);
         // wrapper cude running after the wrapped function
@@ -65,7 +80,14 @@ int main(int argc, char * argv[]) {
     {
         VoidWrapper<decltype(q1)> wrapped_q1(q1);
         wrapped_q1(1);
+
+
         ReturnWrapper<decltype(q2)> wrapped_q2(q2);
         std::cout << "q2(x,y) = " << wrapped_q2(0.1, 0.3) << std::endl;
+
+
+        std::vector<double> x = {1, 2, 3, 4, 5};
+        VoidWrapper<decltype(q3)> wrapped_q3(q3);
+        wrapped_q3(x);
     }
 }
